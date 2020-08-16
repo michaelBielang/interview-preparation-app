2d713be459348bb3e18ba90f59b2597971e26377
Michael Bielang <michael.bielang@hs-augsburg.de>
2020-08-16 19:03:47 +0200

    add security config

--- backend/src/main/java/com/tywdi/backend/configuration/SecurityConfiguration.java
+++ backend/src/main/java/com/tywdi/backend/configuration/SecurityConfiguration.java
@@ -37,8 +37,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.csrf().disable(); // TODO: 15.08.2020
 
         // The pages does not require login
-        http.authorizeRequests()
+        http.authorizeRequests() // TODO: 15.08.2020 deactivate
                 .anyRequest()
                 .permitAll();
+
+        //later
+/*        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("login")
+                .and()
+                .logout().invalidateHttpSession(true).clearAuthentication(true);*/
     }
 }
