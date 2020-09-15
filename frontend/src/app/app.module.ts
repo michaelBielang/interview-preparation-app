6e68ffd9eb68177b376a2fe56e6a9fb76fcbaad5
Michael Bielang <michael.bielang@hs-augsburg.de>
2020-09-15 23:01:12 +0200

    init structure

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
+import {HomeComponent} from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [authInterceptorProviders],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
